137d64ca699bff2ce7f01aefafc3701ec71cd01e
Shahzaib-Jan <shahzaibjan301205@gmail.com>
2026-08-13 12:35:45 +0500

    Time: 0 ms (100%), Space: 8.7 MB (44.63%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        long bForm = n;
+        double ans = 1;
+        if (n < 0)
+        {
+            bForm = -bForm;
+            x = 1/x;
+        }
+        while (bForm > 0)
+        {
+            if (bForm % 2 == 1)
+            {
+                ans *= x;
+            }
+            x *= x;
+            bForm /= 2;
+        }
+        return ans;
+    }
+};
